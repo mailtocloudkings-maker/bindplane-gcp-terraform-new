96bf5a42d89465efbfad278bc49692f01724b1a5
mailtocloudkings-maker <mailtocloudkings@gmail.com>
2026-01-08 23:07:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,4 +3,12 @@ variable "region" {}
 variable "zone" {}
 variable "vm_name" {}
 variable "bucket_name" {}
+
+variable "db_user" {}
+variable "db_pass" {}
+
+variable "bp_admin_user" {}
+variable "bp_admin_pass" {}
+
+variable "bindplane_license_key" {}
 variable "credentials_file" {}
